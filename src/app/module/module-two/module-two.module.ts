bcd00e2db252c78514bf6210dffeb20bd2b041b1
Jefferson Lara <jetox21@gmail.com>
2017-09-25 01:15:27 -0500

    chore(module-two):add export to component and router

--- src/app/module/module-two/module-two.module.ts
+++ src/app/module/module-two/module-two.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { EveLayout } from "../../eve-commons/eve-commons";
+import { ModuleTwoRoutingModule, ModuleTwoComponent } from "./router";
 
 @NgModule({
     imports: [
         BrowserModule,
+        ModuleTwoRoutingModule
     ],
+    exports: [
+        ModuleTwoComponent,
+        ModuleTwoRoutingModule
+    ],
+    declarations: [
+        ModuleTwoComponent
+    ]
 })
 
 @EveLayout({

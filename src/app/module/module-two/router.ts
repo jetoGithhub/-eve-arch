b4bb1559644e545f1ade5673f718f89ab0fd4cf3
Jefferson Lara <jetox21@gmail.com>
2017-09-25 01:16:11 -0500

    chore(module-two): add file router

--- src/app/module/module-two/router.ts
+++ src/app/module/module-two/router.ts
@@ -1,3 +1,22 @@
-/**
- * Created by jetox on 25/09/17.
- */
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from "@angular/router";
+
+@Component({
+    selector:"module-two-component",
+    template:"<h3>Module Two component route</h3>"
+})
+export class ModuleTwoComponent{
+    constructor(){}
+}
+
+const routes: Routes = [
+    { path: 'two-module',  component: ModuleTwoComponent }
+]
+
+@NgModule({
+    imports: [ RouterModule.forRoot(routes) ],
+    exports: [ RouterModule ]
+})
+export class ModuleTwoRoutingModule {
+
+}

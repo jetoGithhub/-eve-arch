2318de6d2994fbbd94344e0d80a0e66ef8a0eb0d
Jefferson Lara <jefferson.arturo.lara.molina.sa@everis.com>
2017-10-02 17:46:44 -0500

    chore(eve-commons): add manager module and restructorto container

--- src/app/eve-commons/eve-commons.ts
+++ src/app/eve-commons/eve-commons.ts
@@ -1,59 +1,17 @@
-export interface ModuleLocation{
-    location:string;
-    description:string;
-    path:string
-}
+import { EveLayoutContainer } from './layout/layout.container';
 
-export interface ContainerLayout{
-    module:string,
-    data:ModuleLocation
-}
+export { ModuleLocation, ContainerLayout } from './layout/layout.interface';
+export { EveLayoutContainer } from './layout/layout.container';
+export { EveLayout } from './layout/layout.decorator';
+export { EveNavbar } from './layout/layout.navbar';
+export { EveSidebar } from './layout/layout.sidebar';
 
-export class EveLayoutContainer{
-    static container:Map<string,ContainerLayout> = new Map();
-    static add(name:string,data:ModuleLocation){
-        if(!this.container.get(name)){
-            this.container.set(name, {
-                module:name,
-                data: data
-            });
-        }
-    }
-
-}
-abstract class BaseEveMennu{
-    protected elementId:string;
-    getItems():Array<ModuleLocation>{
-        let elements = [];
-        EveLayoutContainer.container.forEach((value, key)=>{
-            if(value.data.location.toLocaleLowerCase().trim() === this.elementId){
-                elements.push(value.data);
-            }
-        });
-        return elements;
-    }
-}
-export class EveSidebar extends BaseEveMennu{
-    elementId = 'sidebar';
-
-}
-
-export class EveNavbar extends BaseEveMennu{
-   elementId = 'navbar';
-}
-
-export function EveLayout(data:ModuleLocation){
-    return function (target: Function) {
-        target.prototype.description = data.description;
-        target.prototype.location = data.location;
-        target.prototype.path = data.path;
-
-        EveLayoutContainer.add(target.name, data);
-    }
-}
-
-class EveRouter<T>{
-    constructor(){
-
-    }
+export class ModuleManager {
+  static publish(modules: Array< any > = []): Array< any > {
+    EveLayoutContainer.container.forEach((value) => {
+      modules.push(value.scope);
+    });
+    console.log('imports', modules);
+    return modules;
+  }
 }

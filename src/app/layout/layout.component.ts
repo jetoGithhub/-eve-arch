250a381cb101b0d9700cfd44ab7061ceb93361e3
Jefferson Lara <jefferson.arturo.lara.molina.sa@everis.com>
2017-10-06 11:03:14 -0500

    chore(layout.component): delete component navbar and sdebar

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,33 +1,13 @@
 import { Component } from '@angular/core';
-import { EveNavbar } from '../eve-commons/eve-commons';
 
 @Component({
-    selector:'eve-navbar',
-    template:"<div style='width: 100%; height:64px; top:0;border-bottom: 1px solid black'>" +
-            "<span *ngFor='let list of lists' " +
-                   "style='color: white; background: black; padding: 4px;margin-right: 5px'>" +
-            "<a routerLink='/{{list.path}}'>{{list.description}}</a>" +
-        "</span>&nbsp;&nbsp;&nbsp;&nbsp;" +
-    "</div>"
+    selector: 'eve-layout',
+    template: `<ng-content></ng-content>`
 })
 
-export class NavBarComponent {
-    lists = [];
+export class LayoutComponent {
+
     constructor() {
-        const ENavbar = new EveNavbar();
-        this.lists = ENavbar.getItems();
-        console.log('NAVBAR', this.lists);
-    }
-}
-
-@Component({
-    selector:'eve-layout',
-    template:"<ng-content></ng-content>"
-})
-
-export class LayoutComponent{
-
-    constructor(){
         console.log('layout.module init');
     }
 }

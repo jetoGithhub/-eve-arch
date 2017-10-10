6831cb170a46f4db2ec0ca97e0df54bb524c485f
Jefferson Lara <jefferson.arturo.lara.molina.sa@everis.com>
2017-10-10 17:59:32 -0500

    chore(navbar.component): changed layout navbar to layoutController

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
-import { EveNavbar } from '../../eve-commons/eve-commons';
+import { LayoutController } from '../../eve-commons/eve-commons';
 
 @Component({
   selector: 'eve-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
   providers: [
-    EveNavbar
+    LayoutController
   ]
 })
 
 export class NavBarComponent {
   lists = [];
-  constructor( navbar: EveNavbar ) {
-    this.lists = navbar.getItems();
-    console.log('NAVBAR', this.lists);
+  constructor( private layoutCtrl: LayoutController ) {
+    this.lists = this.layoutCtrl.getItems('navbar');
+    console.log('navbar', this.lists);
   }
 }
